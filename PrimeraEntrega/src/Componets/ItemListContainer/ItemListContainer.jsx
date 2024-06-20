d1d1d0f769f0dd6bf463b25aa30e8e514912e79c
Jadibarre24 <92830984+Jadibarre24@users.noreply.github.com>
2024-06-20 06:56:45 +0000

    rosa

--- PrimeraEntrega/src/Componets/ItemListContainer/ItemListContainer.jsx
+++ PrimeraEntrega/src/Componets/ItemListContainer/ItemListContainer.jsx
@@ -8,7 +8,7 @@ import {
 } from "@chakra-ui/react"
 const ItemListContainer = ({tittle}) => {
   return (
-    <Flex justify={'center'} direction={'row'} fontSize={'x-large'} fontStyle={'normal'} bgColor={'gray'}h={57}>
+    <Flex justify={'center'} direction={'row'} fontSize={'x-large'} fontStyle={'normal'} bgColor={'pink'}h={57}>
       <Box margin={3} >
         <GiChocolateBar />
       </Box>
