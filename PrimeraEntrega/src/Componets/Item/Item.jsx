f19652e296f0d3a68ce87c09a0e023d43e106d7f
jadibarre24 <andy024.jd@gmail.com>
2024-06-24 22:57:23 -0500

    agragaImagenes

--- PrimeraEntrega/src/Componets/Item/Item.jsx
+++ PrimeraEntrega/src/Componets/Item/Item.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Box,Center,Flex } from '@chakra-ui/react'
 
-const Item = ({nombre,precio}) => {
+
+const Item = ({nombre,precio,img,}) => {
   return (
-    <div>
+    <Flex w={300}direction={'column'} alignContent={'Center'}alignItems={'center'} border={'1px'} borderRadius={'6px'}padding={4} margin={2} bg={'#ffd700'}>
+      <img src= {img} width={200} height={200} />
       <h2>{nombre}</h2>
       <h2>{precio}</h2>
-    </div>
+    </Flex>
   )
 }
 
--- PrimeraEntrega/src/data/asyncMock.js
+++ PrimeraEntrega/src/data/asyncMock.js
@@ -1,3 +1,5 @@
+import { Image, Img } from "@chakra-ui/react";
+
 export const productos =[
     {
         id: 1,
@@ -6,30 +8,34 @@ export const productos =[
         clase: 'Chocolates',
         stock: 250,
         descripcion:"Fresas recubiertas de chocolate decoradas para eventos especiales",
+        img: "https://i.postimg.cc/cJrjtCgH/Fresas.jpg",
     },
     {
         id: 2,
-        nombre: "Fresas con chocolate",
-        precio: 2000,
+        nombre: "Caja de Fresas con chocolate",
+        precio: 25000,
         clase: 'Chocolates',
-        stock: 250,
+        stock: 50,
         descripcion:"Fresas recubiertas de chocolate decoradas para eventos especiales",
+        img: "https://i.postimg.cc/fyx1WP3f/caja-de-fresas.jpg",
     },
     {
         id: 3,
-        nombre: "borchetas",
+        nombre: "Brochetas",
         precio: 3000,
         clase: 'galletas',
         stock: 150,
         descripcion:"Fresas recubiertas de chocolate decoradas para eventos especiales",
+        img: "https://i.postimg.cc/3JzDQbXc/IMG-2004.jpg",
     },
     {
         id: 4,
-        nombre: "fuentes",
+        nombre: "Fuentes",
         precio: 150000,
         clase: 'decoracion',
         stock: 2,
         descripcion:"Fresas recubiertas de chocolate decoradas para eventos especiales",
+        img: "https://i.postimg.cc/zXr92qMt/Fuente-Chocolate-Madrid-Goblin.jpg",
     },
     {
         id: 5,
